d94acc2accb296c1f2ee829503c7de5dfa9cd6eb
Dan Tablac <dctablac@gmail.com>
2021-10-21 20:13:41 -0700

    First commit. Restructure files

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Content from '../Content';
+import NavBar from '../NavBar';
+
+export default function App() {
+    return (
+        <div className="App">
+          <NavBar />
+          <Content />
+        </div>
+    );
+}
