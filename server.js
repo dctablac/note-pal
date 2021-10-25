fd7401243046611084a3c1745edfb165e5c69fce
Dan Tablac <dctablac@gmail.com>
2021-10-24 19:20:59 -0700

    Started server file. Began models and routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -7,9 +8,12 @@ app.use(cors());
 app.use(express.json());
 
 // connect to mongoose
-
+mongoose.connect(process.env.DATABASE_URL)
+    .then(() => console.log('Connected to mongodb database.'))
+    .catch((err) => console.error(err))
 // require route
+app.use("/", require('./routes/noteRoute'))
 
 app.listen(3001, () => {
-    console.log('express server running on port 3001')
+    console.log('Express server running on port 3001')
 });
