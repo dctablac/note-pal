6d9a6de683d4b84566b4a30c7f8dc46284cc8fab
Dan Tablac <dctablac@gmail.com>
2021-10-28 18:29:43 -0700

    Corrected build location in server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/", require('./routes/noteRoute'))
 
 // Check if app is deployed
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('frontend/build'));
 }
 
 app.listen(PORT, () => {
