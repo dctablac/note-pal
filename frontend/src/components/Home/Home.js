e30e25499112bc3bf1568b1c543524677fe06e04
Dan Tablac <dctablac@gmail.com>
2021-10-31 18:07:47 -0700

    Added fadeIn styles to pages. Home page title made.

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -3,8 +3,14 @@ import './Home.css';
 
 export default function Home() {
     return (
-        <Fragment>
-            Home Page
-        </Fragment>
+        <div id="home-page" className="flex-container-center">
+            <h1 className="home-title">Note Pal</h1>
+            <div className="home-caption">
+                <p>The</p>
+                <p>handy</p>
+                <p>dandy</p>
+                <p>notepad</p>
+            </div>
+        </div>
     )
 }
